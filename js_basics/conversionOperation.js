1ad47625ffff06bb13defb034b7ba488fc9a8f6f
Kunwar Shaurya <kunwarshaurya28@gmail.com>
2025-05-22 06:52:05 +0000

    conversion operations

---
+++ js_basics/conversionOperation.js
@@ -0,0 +1,91 @@
+let score="33abc"
+let score1=null
+let score2="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+console.log(typeof score);
+console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+
+console.log(typeof score2);
+console.log(typeof(score2));
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=>1; false=>0
+
+
+let Yo=2
+
+let booleanYo=Boolean(Yo)
+console.log(booleanYo);
+
+
+
+// "" => flase
+// "shaurya" => true
+
+
+
+let madira=5
+
+let stringMadira=String(madira)
+
+console.log(stringMadira);
+console.log(typeof stringMadira);
+
+
+
+
+
+//******************operations****************** *
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1="hello"
+let str2=" shaurya"
+
+console.log(str1+str2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log(1+2+"2");
+console.log("1"+(2+2));
+console.log("1"+2+2);
+
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+num1=num2=num3=2+2
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
