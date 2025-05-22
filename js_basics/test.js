00ca46108bfa3da2e4fd702704a3c9f39a941528
Kunwar Shaurya <kunwarshaurya28@gmail.com>
2025-05-22 05:37:28 +0000

    basics

--- js_basics/test.js
+++ js_basics/test.js
@@ -1,1 +1,18 @@
-console.log("shaurya")
+const accountId=144553
+let accountEmail="sahurya28@google.com"
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState;
+
+// accountId=2  //const can not be changed
+
+accountEmail="ckjnk.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+
+
+console.log(accountId)
+
+// prefer not to use var because of issue in bloclk scope and functional scope
+
+console.table([accountId,accountPassword,accountCity, accountState])
